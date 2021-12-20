afb5fd6caa89877a910c9bedac66058c80b61d75
Abid Raza <abid.neduet@gmail.com>
2021-12-20 05:33:05 -0800

    node middlewares

--- practice/node/first-app/middlewares/index.js
+++ practice/node/first-app/middlewares/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
-const logger = require("./third_middleware");
+const logger = require("./third_middleware"); // loading middleware from another file
 var bodyParser = require('body-parser');  
 var urlencodedParser = bodyParser.urlencoded({ extended: false })  
 app.use(express.json());
 
-
+// Custom middlewares
 app.use(function(req, res, next){
     console.log("first middleware ... ");
     next();
 })
-
 
 app.use(function(req, res, next){
     console.log("second middleware ... ");
@@ -19,16 +18,10 @@ app.use(function(req, res, next){
 
 app.use(logger)
 
-
-
-
-
-
 var courses = [{
     name:"DSD",
     year:4
 }]
-
 
 app.post("/addCourse",urlencodedParser, (req, res) => {
 
@@ -46,32 +39,7 @@ app.post("/addCourse",urlencodedParser, (req, res) => {
 })
 
 var server = app.listen(3000)  
-    
-
-// var express = require('express');  
-// var app = express();  
-// var bodyParser = require('body-parser');  
-// // Create application/x-www-form-urlencoded parser  
-// var urlencodedParser = bodyParser.urlencoded({ extended: false })  
-// // app.use(express.static('public'));  
-// // app.get('/index.html', function (req, res) {  
-// //    res.sendFile( __dirname + "/" + "index.html" );  
-// // })  
-// app.post('/process_post', urlencodedParser, function (req, res) {  
-//    // Prepare output in JSON format  
-//    res.send(req.body)
-// //    response = {  
-// //        first_name:req.body.first_name,  
-// //        last_name:req.body.last_name  
-// //    };  
-// //    console.log(response);  
-// //    res.end(JSON.stringify(response));  
-// })  
-// var server = app.listen(8000, function () {  
-// //   var host = server.address().address  
-// //   var port = server.address().port  
-// //   console.log("Example app listening at http://%s:%s", host, port)  
-// })  
+ 
 
 
 
