1035d21844fb5b0332c76f9711a69251fe4f1906
Abid Raza <abid.neduet@gmail.com>
2021-12-24 05:40:44 -0800

    user login

--- practice/node/first-app/authentications/index.js
+++ practice/node/first-app/authentications/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const users = require("./routes/users")
+const auth = require("./routes/auth")
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/users")
@@ -13,6 +14,7 @@ mongoose.connect("mongodb://localhost/users")
 
 app.use(express.json())
 app.use("/api/users",users)
+app.use("/api/auth",auth)
 
 
 const port = process.env.PORT || 3000;
---
+++ practice/node/first-app/authentications/routes/auth.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const {User } = require("../models/user")
+
+
+router.post("/",async (req , res) => {
+    const { error } = validateToLogin(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const userExist = await User.findOne({email:req.body.email});
+    if(!userExist) return res.status(400).send("Invalid email or password");
+
+    const validPassword = await bcrypt.compare(req.body.password , userExist.password);
+    if (!validPassword) return res.status(400).send("Invalid password or email");
+
+    return res.status(200).send(true);
+})
+
+function validateToLogin(User) {
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(50).email().required(),
+        password: Joi.string().min(6).max(255).required()
+    })
+
+    return schema.validate(User)
+}
+
+module.exports = router;
